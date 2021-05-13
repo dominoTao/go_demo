aa32bbc5835b2245a5de774df73e7f5b3060344d
dominoTao <idiotao@163.com>
2021-05-13 23:21:46 +0800

    chan chan-buffer

---
+++ src/ch18/scp/async_service_test.go
@@ -0,0 +1,37 @@
+package scp
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func TestService(t *testing.T) {
+	//fmt.Println(service())
+	//otherTask()
+	asyncService := AsyncService()
+	otherTask()
+	fmt.Println(<-asyncService)
+}
+
+func AsyncService() chan string {
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
